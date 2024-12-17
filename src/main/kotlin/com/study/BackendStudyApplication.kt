e33bae28845ad5b8c2466d649945915d4741ac46
sosow0212 <sosow0212@naver.com>
2024-12-17 20:13:49 +0900

    feat: 프로젝트 기본 설정

---
+++ src/main/kotlin/com/study/BackendStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.study
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackendStudyApplication>(*args)
+}
