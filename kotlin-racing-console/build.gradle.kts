e254d1b55bcb319483bf87cd30aba515bd538f27
sosow0212 <sosow0212@naver.com>
2024-12-20 23:15:23 +0900

    chore: kotest 추가

--- kotlin-racing-console/build.gradle.kts
+++ kotlin-racing-console/build.gradle.kts
@@ -15,10 +15,15 @@ repositories {
     mavenCentral()
 }
 
+val kotestVersion = "5.9.1"
+
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
 }
 
-tasks.test {
+tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
