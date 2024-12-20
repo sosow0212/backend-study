2b96b108bb5a48acc8c23314713a88e6dd0ccb05
sosow0212 <sosow0212@naver.com>
2024-12-20 20:09:51 +0900

    feat: 프로젝트 생성

---
+++ kotlin-racing-console/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+}
+
+group = "com"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
