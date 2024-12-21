86b9e49f5426f3fd69586c0c19bb3b131924c3bc
sosow0212 <sosow0212@naver.com>
2024-12-22 02:02:50 +0900

    feat: 프로젝트 생성

---
+++ kopring-layered-architecture-basic/src/main/kotlin/com/layered/KopringLayeredArchitectureBasicApplication.kt
@@ -0,0 +1,11 @@
+package com.layered
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringLayeredArchitectureBasicApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringLayeredArchitectureBasicApplication>(*args)
+}
